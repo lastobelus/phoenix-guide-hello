8e3b98d55bed1988f5d227e58cca609f654dd594
Michael Johnston <lastobelus@mac.com>
2020-11-24 10:06:22 -0800

    feature: add locale to connection to illustrate defining a module plug
    debug connection attributes verb, host, headers to illustrate adding a
    function plug
    
    add a route/action that extracts a param from an url segment and
    display it in the template.

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -4,4 +4,10 @@ defmodule HelloWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"messenger" => messenger} = params) do
+    # to illustrate how to destructure in signature and still have access to whole params
+    IO.puts("params2: #{inspect(params)}")
+    render(conn, "show.html", messenger: messenger)
+  end
 end

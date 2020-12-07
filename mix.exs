e8df51a0c867ab1288aa2fcdc8133df6634e62d9
Michael Johnston <lastobelus@mac.com>
2020-12-07 09:54:59 -0800

    devops: add os_mon for dashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Hello.MixProject do
   def application do
     [
       mod: {Hello.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 

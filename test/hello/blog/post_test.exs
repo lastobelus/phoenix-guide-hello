dd3545c3b7552b180c6f0ded0a26e98072a9e474
Michael Johnston <lastobelus@mac.com>
2020-12-30 13:35:13 -0800

    test: add a validation to post and a schema test
    that verifies it

---
+++ test/hello/blog/post_test.exs
@@ -0,0 +1,11 @@
+defmodule Hello.Blog.PostTest do
+  @moduledoc false
+
+  use Hello.DataCase, async: true
+  alias Hello.Blog.Post
+
+  test "title must be at least two characters long" do
+    changeset = Post.changeset(%User{}, %{title: "I"})
+    assert %{title: ["should be at least 2 character(s)"]} = errors_on(changeset)
+  end
+end
